f5bde48d3cd046f681eaa8244b150cd944d2d46b
yonghyeon-Jeong <jyh@smartinside.ai>
2022-01-27 10:16:21 +0900

    Create new Branch

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ print('Initial commit')
 print('Second commit from B')
 print('Third commit from A(Pull)')
 print('Fourth commit from B')
+# --------------------------------------------------------------
+print('Create new Branch')
